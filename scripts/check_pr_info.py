efaaa909644aa9a18eaf105f93bc6939f3c6e390
madhulmk <mkmadhul@outlook.com>
2026-09-02 11:27:31 +0000

    change Github Api print PR list

--- scripts/check_pr_info.py
+++ scripts/check_pr_info.py
@@ -19,4 +19,5 @@ class GetPRInfo:
                 "closed_at": pr.get("closed_at"),
             }
             pr_info_list.append(pr_info)
+        print(f"Pull request info for {owner}/{repo}: {pr_info_list}")
         return pr_info_list
