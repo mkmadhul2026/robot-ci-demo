b9613c6ad7bdda369feb8348b6c2076fc420146b
madhulmk <mkmadhul@outlook.com>
2026-09-02 12:03:39 +0000

    change Github Api output to GHA GHA fix1

--- scripts/check_pr_info.py
+++ scripts/check_pr_info.py
@@ -5,7 +5,7 @@ print("Starting PR info check...")
 class GetPRInfo:
     def __init__(self, token):
         self.github_api = GithubApi(token)
-        print
+        print("Initialized GetPRInfo with provided token.", flush=True)
 
     def get_pull_request_info(self, owner, repo):
         pull_requests = self.github_api.get_repo_pull_requests(owner, repo)
